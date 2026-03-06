5ffcddd8bd68d1ab965ce346e51b5c1ac11dc8e4
rifat <rohanrifat122@gmail.com>
2026-03-06 10:49:40 +0600

    update chat file

--- Js/chat.js
+++ Js/chat.js
@@ -2,18 +2,30 @@ function sendMessage(){
 
 let msg = document.getElementById("message").value;
 
+/* empty message check */
+if(msg.trim() === ""){
+return;
+}
+
 fetch("send_message.php",{
 
 method:"POST",
+
 headers:{
 "Content-Type":"application/x-www-form-urlencoded"
 },
 
-body:"message="+msg+"&receiver="+receiver
+body:"message="+encodeURIComponent(msg)+"&receiver="+receiver
+
+})
+.then(res => res.text())
+.then(data => {
+
+document.getElementById("message").value="";
+loadMessages(); // reload chat after send
 
 });
 
-document.getElementById("message").value="";
 }
 
 function loadMessages(){
@@ -30,4 +42,8 @@ document.getElementById("chat-box").innerHTML=data;
 
 }
 
+/* load messages immediately */
+loadMessages();
+
+/* auto refresh every second */
 setInterval(loadMessages,1000);
