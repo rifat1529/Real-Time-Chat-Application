93bcce5e357b3ab39c1bda5be64de39df8996e4c
rifat <rohanrifat122@gmail.com>
2026-03-08 02:39:04 +0600

    update get-messages

--- get_messages.php
+++ get_messages.php
@@ -15,9 +15,18 @@ $result = mysqli_query($conn,$sql);
 while($row = mysqli_fetch_assoc($result)){
 
     if($row['sender_id'] == $sender){
-        echo "<div class='msg-sent'>".htmlspecialchars($row['message'])."</div>";
+
+        echo "<div class='msg-sent'>
+                ".htmlspecialchars($row['message'])."
+                <button class='delete-btn' onclick='deleteMessage(".$row['id'].")'>Delete</button>
+              </div>";
+
     }else{
-        echo "<div class='msg-received'>".htmlspecialchars($row['message'])."</div>";
+
+        echo "<div class='msg-received'>
+                ".htmlspecialchars($row['message'])."
+              </div>";
+
     }
 
 }
